b8fa3606d9528a4bb42935f9a5920b82eee49f39
Anna Dang <annadang2001@gmail.com>
2025-01-08 02:33:34 +1100

    Implement get functions based on product properties

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.use(bodyParser.json());
 const products = [
     { 
       id: 1, 
-      name: 'Korean Toner', 
+      name: 'Korean Beauty of Josean Toner', 
       country: 'Korean', 
       brand: 'Anua', 
       type: 'Toner',
-      ingredients: 'Niacinamide', 
+      ingredients: ['Niacinamide'], 
       skin_type: ['Oily', 'Dry'], 
-      skin_concerns: ['Acne, Hyperpigmentation'], 
+      skin_concerns: ['Acne', 'Hyperpigmentation'], 
       price: 25, 
       link: 'https://www.sephora.sg/products/fresh-soy-face-cleanser/v/400ml' 
     },
@@ -29,9 +29,9 @@ const products = [
       country: 'Japanese', 
       brand: 'Biore', 
       type: 'Sunscreen',
-      ingredients: 'Hyaluronic Acid', 
+      ingredients: ['Hyaluronic Acid'], 
       skin_type: ['Oily', 'Dry'],
-      skin_concerns: 'Sun Damage', 
+      skin_concerns: ['Sun Damage'],
       price: 18, 
       link: 'https://www.sephora.sg/products/shiseido-ultimate-sun-protector-lotion-spf-50/v/150ml' 
     },
@@ -41,9 +41,9 @@ const products = [
       country: 'French', 
       brand: 'La Roche-Posay', 
       type: 'Moisturizer',
-      ingredients: 'Ceramides, Niacinamide', 
+      ingredients: ['Ceramides', 'Niacinamide'], 
       skin_type: ['Sensitive'], 
-      skin_concerns: 'Redness, Dryness', 
+      skin_concerns: ['Redness', 'Dryness'], 
       price: 35, 
       link: 'https://www.sephora.sg/products/la-roche-posay-toleriane-sensitive-cream/v/40ml' 
     },
@@ -65,16 +65,80 @@ app.get('/products/id/:id', (req, res) => {
   }
 });
 
+// Get product by name
+// http://localhost:8080/products/name/Korean%20Toner
+app.get('/products/name/:name', (req, res) => {
+  const searchTerms = req.params.name.split(' ');
+  const product = products.filter((p) => searchTerms.every((term) => p.name.includes(term)));
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  }
+});
 
-// Get product by Skin Type
+// Get product by type
+app.get('/products/type/:type', (req, res) => {
+  const product = products.find((p) => p.type === req.params.type);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  }
+});
+
+// Get product by skin type
 app.get('/products/skin_type/:skin_type', (req, res) => {
-    const product = products.filter((p) => p.skin_type.includes(req.params.skin_type));
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).json({ message: 'Product not found' });
-    }
-  });
+  const product = products.filter((p) => p.skin_type.includes(req.params.skin_type));
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  }
+});
+
+// Get product by ingredient
+app.get('/products/ingredients/:ingredient', (req, res) => {
+  const product = products.filter((p) => p.ingredients.includes(req.params.ingredient));
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  }
+});
+
+// Get product by brand
+app.get('/products/brand/:brand', (req, res) => {
+  const product = products.find((p) => p.brand === req.params.brand);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  }
+});
+
+// Get product by skin concern
+app.get('/products/skin_concerns/:skin_concern', (req, res) => {
+  const product = products.filter((p) => p.skin_concerns.includes(req.params.skin_concern));
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  }
+});
+
+// Get product by price
+// http://localhost:8080/products/price?min=0&max=30
+app.get('/products/price', (req, res) => {
+  const min = parseFloat(req.query.min);
+  const max = parseFloat(req.query.max);
+  const product = products.filter((p) => p.price <= max && p.price >= min);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  }
+});
 
 // Create a new product
 app.post('/products', (req, res) => {
